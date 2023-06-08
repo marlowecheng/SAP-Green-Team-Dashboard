29b61982433314055d6ebbbe12338858d3cb2e06
marlowe cheng <114950923+marlowecheng@users.noreply.github.com>
2023-06-08 11:41:26 -0700

    removed banner assets and code from app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './style/App.css';
 
-import { Card, Metric, Text, Title, BarChart, Subtitle, AreaChart, DonutChart, Flex, BadgeDelta, Badge, BarList, Bold} from "@tremor/react";
+import { Card, Metric, Text, Title, BarChart, Subtitle, AreaChart, DonutChart, Flex, BadgeDelta, Badge, BarList, Bold} from '@tremor/react';
 import { Carousel, Typography, IconButton, Chip, Navbar, MobileNav, Button, } from '@material-tailwind/react';
-import { ChevronLeftIcon, ArrowRightIcon, ChevronRightIcon, ArrowUpIcon, ArrowDownIcon } from "@heroicons/react/24/outline";
+import { ChevronLeftIcon, ArrowRightIcon, ChevronRightIcon, ArrowUpIcon, ArrowDownIcon } from '@heroicons/react/24/outline';
 import { Icon } from '@tremor/react/dist';
 
-import { Header } from './layouts/Header';
+import Header from './layouts/Header';
 import Footer from './layouts/Footer';
+
+import Banner from './components/Banner';
 
 import cascadeImg from './assets/cascade-recovery.svg';
 import employeePresence from './assets/Employees.svg';
@@ -15,14 +17,9 @@ import napkinHero from './assets/NapkinHeroV2.png';
 import ddaLogo from './assets/DDALOGO.png';
 import anishaCard from './assets/Anisha_CardPhoto.jpg';
 import footerLogo from './assets/SAPVECTORLOGO.svg';
-import cascadeImage from './assets/cascade-recovery-lineup.svg';
-import ecoActionImage from './assets/eco-action-lineup.svg';
-import refundableImage from './assets/refundables-lineup.svg';
-import wasteImage from './assets/waste-management-lineup.svg';
 import ecoServiceCard from './assets/eco-action.svg';
 import wasteServiceCard from './assets/wastemanagement.svg';
 import refundServiceCard from './assets/refundables.svg';
-import heroBackground from './assets/SAPVECTORLOGO-COLOR.svg';
 
 
   //Creating an array for the data to be inputted into the 'Solid Waste Diversion' Card
@@ -42,61 +39,7 @@ import heroBackground from './assets/SAPVECTORLOGO-COLOR.svg';
 
     <Header />
 
-    <div className='bg-bgmain py-4 flex justify-evenly items-center border-b-[60px] border-textmain'>
-      <div className='flex flex-col'>
-        <h1 className='text-textmain font-bold text-5xl z-10'>Reveal the Green</h1>
-        <h1 className='text-textmain font-bold text-5xl z-10'>Team Revolution</h1>
-        <h2 className='text-textmain text-2xl z-10'>Witness the Journey from Waste to sustainability.</h2>
-        <div className='absolute top-1 right-0 left-[-50px] bottom-0'>
-          <img src={heroBackground} className='w-[550px] h-[550px] z-0 opacity-10'/>
-        </div>
-      </div>
-        
-      
-      <div classname='flex flex-row'>
-        <div className='flex flex-row my-4 gap-x-4'>
-          {/* Hero Card Cascade Recovery */}
-          <Card className="w-[255px] flex flex-col items-center border-cascadeBorder border-[5px] drop-shadow-xl">
-            <h1 className='text-textmain font-bold'>Cascade Recovery</h1>
-            <img src={cascadeImage} className='my-3'/>
-            <div className='w-32 h-12 rounded-full bg-pillBgGreen bg-opacity-[40%] text-center flex flex-row items-center justify-center gap-x-2 drop-shadow-2xl'>
-                <ArrowUpIcon className='text-green-900 stroke-[3px] w-6 h-6'/>
-                <p className='font-bold text-lg text-green-900'>45%</p>
-            </div>
-          </Card>
-          {/* Hero Card Eco Action */}
-          <Card className="w-[255px] flex flex-col items-center border-ecoBorder border-[5px] drop-shadow-xl">
-            <h1 className='text-textmain font-bold'>Eco Action</h1>
-            <img src={ecoActionImage} className='my-3'/>
-            <div className='w-32 h-12 rounded-full bg-pillBgGreen bg-opacity-[40%] text-center flex flex-row items-center justify-center gap-x-2 drop-shadow-2xl'>
-                <ArrowUpIcon className='text-green-900 stroke-[3px] w-6 h-6'/>
-                <p className='font-bold text-lg text-green-900'>45%</p>
-            </div>
-          </Card>
-        </div>
-
-        <div className='flex flex-row my-4 gap-x-4'>
-          {/* Hero Card Waste Control Services */}
-          <Card className="w-[255px] flex flex-col items-center border-wasteBorder border-[5px] drop-shadow-xl">
-            <h1 className='text-textmain font-bold'>Waste Control Services</h1>
-            <img src={wasteImage} className='my-3'/>
-            <div className='w-32 h-12 rounded-full bg-pillBgRed bg-opacity-[40%] text-center flex flex-row items-center justify-center gap-x-2 drop-shadow-2xl'>
-                <ArrowDownIcon className='text-red-900 stroke-[3px] w-6 h-6'/>
-                <p className='font-bold text-lg text-red-900'>45%</p>
-            </div>
-          </Card>
-          {/* Hero Card refundables */}
-          <Card className="w-[255px] flex flex-col items-center border-refundBorder border-[5px] drop-shadow-xl">
-            <h1 className='text-textmain font-bold'>Refundables</h1>
-            <img src={refundableImage} className='my-3' />
-            <div className='w-32 h-12 rounded-full bg-pillBgGreen bg-opacity-[40%] text-center flex flex-row items-center justify-center gap-x-2 drop-shadow-2xl'>
-                <ArrowUpIcon className='text-green-900 stroke-[3px] w-6 h-6'/>
-                <p className='font-bold text-lg text-green-900'>45%</p>
-            </div>
-          </Card> 
-        </div>
-      </div>
-    </div>
+    <Banner />
 
     <div className='flex flex-row items-center justify-evenly m-10'>
       {/* Office Presence Card */}
