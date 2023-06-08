840b3dfa67ea770fce6766547d647b388f159ef1
marlowe cheng <114950923+marlowecheng@users.noreply.github.com>
2023-06-08 14:23:16 -0700

    commenting out nav btns

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -9,14 +9,14 @@ export const Header = () => {
           className="w-36 h-30"
         />
       </div>
-      <div className="mb-4">
+      {/* <div className="mb-4">
         <button className="rounded-full w-32 h-10 mt-4 border-4 border-insightBtn bg-white text-insightBtn mr-4">
           Home
         </button>
         <button className="rounded-full w-32 h-10 mt-4 bg-insightBtn text-white mr-10">
           Insights
         </button>
-      </div>
+      </div> */}
     </div>
   );
 };
