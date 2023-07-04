6005716e297dcbcddb23dbe9b7ea82b0c80035b4
davekoo1106 <114951120+davekoo1106@users.noreply.github.com>
2023-07-03 21:26:22 -0700

    Created a test array

--- src/components/TestPage.js
+++ src/components/TestPage.js
@@ -3,11 +3,31 @@
 
 import React, { useState, useEffect } from 'react';
 import year2023 from '../data/monthlyData2023.json';
+import data from '../data/data.json';
 
 // console.log(year2023)
 
+
+const items = [
+    {id: 1, name: 'Cascade Recovery', description: 'Cascade Recovery Services' },
+    {id: 1, name: 'Coffee Grounds', description: 'Coffee Grounds Waste Removal'}
+]
+
+// const Items = ({ items }) => {
+//     return (
+//         <div>
+//             <h1>
+//                 {items.map((item) => (
+//                     <Item/>
+//                 ))};
+//             </h1>
+//         </div>
+//     )
+// }
+
+
 const TestData = () => {
-    const [ data, setData ] = useState([]);
+    const [ data, setData ] = useState(true);
     const year2023 = data;
 
     console.log(year2023)
@@ -17,7 +37,7 @@ const TestData = () => {
         const dataFetch = async () => {
             try {
                 const response = await fetch('../data/monthlyData2023.json');
-                const json = await response.json()
+                const json = await response.json();
                 setData(json.data);
             } catch (error) {
                 console.log('Error Fetching Data: ', error);
@@ -30,16 +50,15 @@ const TestData = () => {
 
     return (    
         <div>
-            {year2023.map((item, index) => (
-                <div key={index}>
-                    <h1 className='text-red-400'>{item}DATA HERE</h1>
-                    {/* Access specific properties if needed */}
-                    <h2 className='text-red-400'>{item.garbage}DATA HERE</h2>
-                </div>
-            ))}
+            <div>
+                <h1 className='text-red-400'>{}DATA HERE</h1>
+                {/* Access specific properties if needed */}
+                <h2 className='text-red-400'>{}DATA HERE</h2>
+            </div>
         </div>
     )
 }
+
 
 export default TestData; 
 
