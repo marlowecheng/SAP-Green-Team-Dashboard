6c88b85f901d5a8633cf781af88f1cc65bd45aa3
davekoo1106 <114951120+davekoo1106@users.noreply.github.com>
2023-07-17 17:41:25 -0600

    working on passing data into the employee presence pill

--- src/components/OfficePresence.js
+++ src/components/OfficePresence.js
@@ -2,8 +2,15 @@ import { Card } from "@tremor/react";
 import { ArrowUpIcon } from "@heroicons/react/24/outline";
 
 import employeePresence from "../assets/Employees.svg";
+import employeePresenceData from "../data/employeePresence.json";
+import { useState } from "react";
+
+
 
 function OfficePresence() {
+  
+  const [employeeData, setEmployeeData] = useState('') 
+  
   return (
     <div>
       <h1 className="text-center mb-2 font-bold text-textmain text-xl">
@@ -19,9 +26,11 @@ function OfficePresence() {
           />
         </div>
         <div>
-          <h1 className="text-xl text-indigo-600 font-bold mb-4">May</h1>
+          {/* Insert Month Data Here */}
+          <h1 className="text-xl text-indigo-600 font-bold mb-4">July</h1>
           <div className="w-40 h-14 rounded-full bg-pillBgGreen text-center flex flex-row items-center justify-evenly">
             <ArrowUpIcon className="text-white stroke-[4px] w-6 h-6" />
+          {/* Insert Percentage Data Here */}
             <p className="text-white font-bold text-2xl">50.1%</p>
           </div>
           <div className="mt-4">
