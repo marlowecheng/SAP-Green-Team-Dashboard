4a469e9579a96f7299936a22a73b07943fdcf747
marlowe cheng <114950923+marlowecheng@users.noreply.github.com>
2023-07-18 08:15:21 -0700

    created comments explaining the code

--- src/components/OfficePresence.js
+++ src/components/OfficePresence.js
@@ -3,24 +3,30 @@ import { ArrowUpIcon, ArrowDownIcon } from "@heroicons/react/24/outline";
 
 import employeePresence from "../assets/Employees.svg";
 import employeePresenceData from "../data/employeePresence.json";
-import { useState } from "react";
 
 function OfficePresence({ displayMonth }) {
     
+	// Find the array set that matches with the month passed in
 	const currMonthData = employeePresenceData.EMPLOYEE.find((item) => item.month === displayMonth);
 
+	// Get the value of 'count' key
 	const currMonthCount = currMonthData.count;
 
+	// Get the index number of the current array set
 	const currMonthIndex = employeePresenceData.EMPLOYEE.findIndex((item) => item.month === displayMonth);
 
+	// Set the index of previous month back 1
 	const prevMonthIndex = currMonthIndex - 1;
 
+	// Empty variable to pass the previous month's count number into
 	let prevMonthCount = 0;
 
+	// Get the previous month's count number if the previous month index is equal or greater than 0
 	if (prevMonthIndex >= 0) {
 		prevMonthCount = employeePresenceData.EMPLOYEE[prevMonthIndex].count;
 	};
 
+	// Calculate the difference between the two months and output as a percentage
 	const presencePercentage = Math.round(((currMonthCount - prevMonthCount) / currMonthCount) * 100);
 
 	return (
@@ -42,6 +48,7 @@ function OfficePresence({ displayMonth }) {
                     <h1 className="text-xl text-indigo-600 font-bold mb-4">
 						{displayMonth.toUpperCase()}
                     </h1>
+					{/* If the presencePercentage is more than 0, display the pill as green with an up arrow */}
 					{presencePercentage > 0 && (
 							<div className="w-40 h-14 rounded-full bg-pillBgGreen text-center flex flex-row items-center justify-evenly">	
                         		<ArrowUpIcon className="text-white stroke-[4px] w-6 h-6" />
@@ -49,6 +56,7 @@ function OfficePresence({ displayMonth }) {
                        			<p className="text-white font-bold text-2xl">{presencePercentage}%</p>
                     		</div>
 					)}
+					{/* If the presencePercentage is less than 0, display the pill as red with a down arrow */}
 					{presencePercentage < 0 && (
 						<div className="w-40 h-14 rounded-full bg-pillBgRed text-center flex flex-row items-center justify-evenly">	
 						<ArrowDownIcon className="text-white stroke-[4px] w-6 h-6" />
